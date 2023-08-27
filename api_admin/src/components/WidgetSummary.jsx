c6fe9ee92cc0a654b8111c81dea191ccbebf0276
teebrown101 <tundebrown101@outlook.com>
2023-08-27 21:47:28 +0100

    resolved Iconify path

--- api_admin/src/components/WidgetSummary.jsx
+++ api_admin/src/components/WidgetSummary.jsx
@@ -5,7 +5,7 @@ import { Card, Typography } from '@mui/material';
 // utils
 import { fShortenNumber } from '../utils/formatNumber';
 // components
-import Iconify from './iconify';
+import Iconify from './Iconify';
 
 // ----------------------------------------------------------------------
 
